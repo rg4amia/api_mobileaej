cdf8e17e7c7326d7613af6f290a7d061b85a6088
AMIA Thierry Stephane <r4gamia@gmail.com>
2022-11-03 09:47:37 +0000

    change secteuractivite model to digit_parametrage_secteuractivite

--- app/Models/SecteurActivite.php
+++ app/Models/SecteurActivite.php
@@ -7,8 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class SecteurActivite extends Model
 {
 
-  //local ://  public $table = 'digit_parametrage_secteuractivite';
-    public $table = 'digit_parametrage_secteuractivite1';
+  //local ://  public $table = 'digit_parametrage_secteuractivite1';
+    public $table = 'digit_parametrage_secteuractivite';
 
     public function soussecteuractivites(){
         return $this->hasMany('App\Models\SousSecteur','secteuractivite_id','id');
